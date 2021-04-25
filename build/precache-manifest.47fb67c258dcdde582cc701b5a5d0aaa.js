39af7a1b3f9c4feabb6f284351e0240147ceba35
Damian Dusza <damian.dusza@madmountain.io>
2021-04-25 13:29:09 +0200

    build

---
+++ build/precache-manifest.47fb67c258dcdde582cc701b5a5d0aaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b202588a743140f232a3c71dbb204dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88f12610ec18e16fa8b4",
+    "url": "/static/css/main.165011c6.chunk.css"
+  },
+  {
+    "revision": "761eb827099cbe30cfd2",
+    "url": "/static/js/2.2f270463.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2f270463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f12610ec18e16fa8b4",
+    "url": "/static/js/main.c89ac022.chunk.js"
+  },
+  {
+    "revision": "d7821f64d7774857adf8",
+    "url": "/static/js/runtime-main.15e8976b.js"
+  }
+]);
